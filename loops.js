026b137e472da846d67a6f3f536efb53e1702dc3
savita madray <savitamadray01@gmail.com>
2017-10-05 23:39:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,7 @@
-function forLoop(array){
 
-  for (let i = 0; i < 25; i++) {
-    if(i===1){
-      array.push(`"I am ${i} strange loop"`)
-    }
-    else {
-      array.push(`"I am ${i} strange loops"`)
-    }
-    return [array]
-}
+ function forLoop(array) {
+ for (let i = 0; i < 25 ; i++) {
+     array.push('I am ${i} strange loop ${i===0 ? \'\' : \'s\' }.')
+   }
+  return array;
 }
